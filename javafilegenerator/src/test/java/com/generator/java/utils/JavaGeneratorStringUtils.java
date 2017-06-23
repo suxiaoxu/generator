f3d83a16474a064275ab7a19a0de1cc2da6167cb
suxiaoxu <348586687@qq.com>
2017-06-23 23:49:15 +0800

    增加修改

---
+++ javafilegenerator/src/test/java/com/generator/java/utils/JavaGeneratorStringUtils.java
@@ -0,0 +1,12 @@
+package com.generator.java.utils;
+
+/**
+ * Created by Administrator on 2017/6/23 0023.
+ */
+public class JavaGeneratorStringUtils {
+
+    public static String headUpCase(String name){
+
+        return name;
+    }
+}
